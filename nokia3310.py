c3df51ce847a82617c9168d906cdb1215ef6106c
bally3399 <151347697+bally3399@users.noreply.github.com>
2023-12-04 15:40:24 +0100

    Add files via upload

---
+++ nokia3310.py
@@ -0,0 +1,271 @@
+while True:
+    print("""
+    list of menu functions
+    1. phone Book
+    2. Message
+    3. Chat
+    4. Call Register
+    5. Tones
+    6. Settings
+    7. Call Setting
+    8. Game
+    9. Calculator
+    10. Reminder
+    11. clock
+    12. Profile
+    13. Sim Services
+    """)
+    user_input = int(input("press number from 1 to 13: "))
+    if user_input == 0:
+       print("Existing the Menu App")	
+       break
+    elif user_input == 1:
+       print("""
+       List of phone Book
+       1. Search 
+       2. Service Nos
+       3. Add name
+       4. Erase
+       5. Edit
+       6. Assign tone
+       7. Send b'card	
+       8. Options
+       9. Speed dials
+       10. Voice tags
+       """)
+    user_input_a = int(input("Press a number: "))
+    if user_input_a == 0:
+       break
+    elif user_input_a == 8:
+       print("""
+       1. Type of view
+       2. Memory status
+       """)
+    user_input_b = int(input("Press a number: "))
+    if user_input_b == 0:
+       break
+    elif user_input_b == 2:
+       print("""
+       list of message
+       1. Write messages
+       2. inbox
+       3. outbox
+       4. Picture messages
+       5. Templates
+       6. Smileys
+       7. Message Settings
+       8. Info service
+       9. Voice mailbox number
+       10. Service Command editor
+       """)
+    user_input_c = int(input("Press a number: "))
+    if user_input_c == 0:
+       break
+    elif user_input_c == 7:
+       print("""
+       1. Set
+       2. Common
+	""")
+    user_input_d = int(input("press a number: "))
+    if user_input_d == 0:
+       break
+    elif user_input_d == 1:
+       print("""
+       1. Message centre number
+       2. Message sent as
+       3. Message validity
+       """)
+    user_input_e = int(input("Press a number: "))
+    if user_input_e == 0:
+       break
+    elif user_input_e == 2:
+       print("""
+       1. Delivery
+       2. Reply via same centre
+       3. Character
+       """)
+    user_input_e = int(input("press a number: "))
+    if user_input_e == 0:
+       break
+    elif user_input_e == 3:
+       print("""
+       1. Chat
+       """)
+    user_input_e = int(input("Press a number"))
+    if user_input_e == 0:
+       break
+    elif user_input_e == 4:
+       print("""
+       list of Call register
+       1. Missed calls
+       2. Received calls 
+       3. Dialled numbers
+       4. Erase recent call lists
+       5. Show call duration
+       6. Show call Cost
+       7. Call cost settings
+       8. Prepaid credit
+       """)
+    user_input = int(input("Press a number: "))
+    if user_input == 0:
+       break
+    elif user_input == 5:
+       print("""
+       1. last call duration
+       2. All calls'duration
+       3. Received calls'duration
+       4. Dialled calls' duration
+       5. clear timers
+       """)
+    user_input_e = int(input("Press a number: "))
+    if user_input_e == 0:
+       break
+    elif user_input_e == 6:
+       print("""
+       1. Last call cost
+       2. All call's cost
+       3. Clear counters
+       """)
+    user_input_e = int(input("Press a number: "))
+    if user_input_e == 0:
+       break
+    elif user_input_e == 7:
+       print("""
+       1. Call cost limit
+       2. Show costs in
+       """)
+    user_input_e = int(input("Press a number: "))
+    if user_input_e == 0:
+       break
+    elif user_input_e == 8:
+       print("""
+       1. prepaid credit
+       """)
+    user_input_f = int(input("Press a number: "))
+    if user_input_f == 0:
+       break
+    elif user_input_f == 5:
+       print("""
+       1. Ringing tone
+       2. Ringing
+       3. Incoming call alert
+       4. Composer
+       5. Message alert tone
+       6. Keypad tones
+       7. Warning and game tones
+       8. Vibrating alert
+       9. Screen saver 
+       """)
+    user_input_g = int(input("Press a number: "))
+    if user_input_g == 0:
+       break
+    elif user_input_g == 6:
+       print("""
+       List of Settings
+       1. Call settings
+       2. Phone Settings
+       3. Security settings
+       4. Restore factory settings
+       """)
+    user_input_h = int(input("Press a number: "))
+    if user_input_h == 0:
+       break
+    elif user_input_h == 1:
+       print("""
+       List of call settings
+       1. Automatic redial
+       2. Speed dialling
+       3. Call waiting options
+       4. Own number sending
+       5. Phone line in use 
+       6. Automatic answer
+       """)
+    user_input_h = int(input("Press a number: "))
+    if user_input_h == 0:
+       break
+    elif user_input_h == 2:
+       print("""
+       1. language
+       2. Cell info display
+       3. Welcome 
+       4. Network selection
+       5. Light
+       6. Confirm SIM service actions
+       """)
+    user_input_h = int(input("Press a number: "))
+    if user_input_h == 0:
+       break
+    elif user_input_h == 3:
+       print("""
+       1. PIN code request
+       2. Call barring service 
+       3. Fixed dialling
+       4. Closed user group
+       5. Phone security
+       6. Change access codes 
+       """)
+    user_input_h = int(input("Press a number: "))
+    if user_input_h == 0:
+       break
+    elif user_input_h == 4:
+       print("""
+       Restore factory settings
+       """)
+    user_input_i = int(input("Press a number: "))
+    if user_input_i == 0:
+       break
+    elif user_input_i == 7:
+       print("""
+       Call divert
+       """)
+    user_input_j = int(input("Press a number: "))
+    if user_input_j == 0:
+       break
+    elif user_input_j == 8:
+       print("""
+       Game
+       """)
+    user_input_k = int(input("Press a number: "))
+    if user_input_k == 0:
+       break
+    elif user_input_k == 9:
+       print("""
+       Calculator
+       """)
+    user_input_l = int(input("Press a number: "))
+    if user_input_l == 0:
+       break
+    elif user_input_l == 10:
+       print("""
+       Reminder
+       """)
+    user_input_m = int(input("Press a number: "))
+    if user_input_m == 0: 
+       break
+    elif user_input_m == 11:
+       print("""
+       1. Alarm clock
+       2. Clock Settings
+       3. Date settings 
+       4. Stopwatch
+       5. Countdown timer
+       6. Auto update of date and time
+       """)
+    user_input_n = int(input("Press a number: "))
+    if user_input_n == 0:
+       break
+    elif user_input_n == 12:
+       print("""
+       profile
+       """)
+    user_input_o = int(input("Press a number: "))
+    if user_input_o == 0:
+       break
+    elif user_input_o == 13:
+       print("""
+       SIM service
+       """)
+	  
+
+
+       
